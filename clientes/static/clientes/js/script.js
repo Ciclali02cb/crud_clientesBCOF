a9444154ea72c675cff42c0753c0dac9d1358453
Ciclali Ortiz <ciclaliortiz02@gmail.com>
2024-12-02 00:44:21 -0600

    “Cambios_INDEX_2”

--- clientes/static/clientes/js/script.js
+++ clientes/static/clientes/js/script.js
@@ -44,10 +44,10 @@ document.querySelectorAll('.navar a[href^="#"]').forEach(function(enlace){
 
 //Cambiar la imagen de fondo cada cierto segundo
 const imagenes = [
-    '/static/clientes/img/fondo.png',
-    '/static/clientes/img/bienestar.jpg',
-    '/static/clientes/img/fondo3.jpg',
-    '/static/clientes/img/bien.jpg'
+    '/static/clientes/IMG/fondo.png',
+    '/static/clientes/IMG/bienestar.jpg',
+    '/static/clientes/IMG/fondo3.jpg',
+    '/static/clientes/IMG/bien.jpg'
 ];
 const homeSection = document.querySelector('.home');
 const intervalo = 5000; //5000 ms=5s
